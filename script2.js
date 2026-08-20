a621e76420fdd58ca0af872a8de9b366f1dd54f4
idracsib <biscardialicia@gmail.com>
2026-08-20 20:59:38 -0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,27 @@
+document.getElementById('numeroForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    // Obtém o valor do número digitado
+    const numero = parseFloat(document.getElementById('numero').value);
+
+    // Obtém a operação selecionada
+    const operacao = document.getElementById('operacao').value;
+
+    let resultado;
+    if (operacao === "quadrado") {
+        resultado = calcularQuadrado(numero);
+    } else if (operacao === "cubo") {
+        resultado = calcularCubo(numero);
+    }
+
+    // Exibe o resultado na página
+    document.getElementById('resultado').textContent = resultado;
+});
+
+function calcularQuadrado(numero) {
+    return numero * numero;
+}
+
+function calcularCubo(numero) {
+    return numero * numero * numero;
+}
